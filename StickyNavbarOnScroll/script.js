b013fc0b3d5a5d903eeb5b2e6efeaff2630cf71b
DESKTOP-E141O5H\Vijju <imvijju94negi@gmail.com>
2023-02-28 13:33:12 +0530

    Active  Menu Switcher

--- StickyNavbarOnScroll/script.js
+++ StickyNavbarOnScroll/script.js
@@ -2,3 +2,15 @@ window.addEventListener("scroll", () => {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+// active
+
+const navbarNav = document.querySelector(".navbar-nav");
+navbarNav.addEventListener("click", (e) => {
+  const navLink = e.target.parentElement;
+  if (navLink.classList.contains("nav-item")) {
+    // alert("hello");
+    navbarNav.querySelector(".active").classList.remove("active");
+    e.target.parentElement.classList.add("active");
+  }
+});
